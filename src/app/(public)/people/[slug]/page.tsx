1d1db1f301b189ec9a9ae7a9fa9e8a9eaf9837a4
Jose Sandoya <joseivansandoya@gmail.com>
2023-08-25 11:52:27 -0500

    dynamic routes

---
+++ src/app/(public)/people/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function Person({ params }: { params: { slug: string } }) {
+  return <div>Person: {params.slug}</div>
+}
+
+export async function generateStaticParams() {
+  const people = ['luke-skywalker', 'leia-organa', 'han-solo']
+
+  return people.map((slug) => ({ slug }))
+}
